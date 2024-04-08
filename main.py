0d5566c44cdd17ae4e89b143ca43cce7ce3c2c52
NatesDesuu <athankun8@gmail.com>
2024-04-08 18:39:12 +0800

    Add starting files

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, abort, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
